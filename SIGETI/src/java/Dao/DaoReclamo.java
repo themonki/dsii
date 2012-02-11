70e878016f4094e6588f0344e68828dd2d22e220
yermigon@gmail.com <yermigon@gmail.com>
2012-02-11 14:20:12 -0500

    cambios reclamos bean dao y controlador

--- SIGETI/src/java/Dao/DaoReclamo.java
+++ SIGETI/src/java/Dao/DaoReclamo.java
@@ -6,9 +6,10 @@ package Dao;
 import Entidades.Reclamo;
 import Utilidades.FachadaBD;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import javax.swing.JOptionPane;
+
 
 /**
  *
@@ -23,29 +24,64 @@ public class DaoReclamo {
     }
     
      public int saveReclamo(Reclamo reclamo) {
-        String sql_insert = "INSERT INTO bus (matricula,tipo,"
-                + "capacidad,id_interno,estado,pertenece_ruta";
+        String sql_insert = "INSERT INTO reclamo (ticket,fecha,"
+                + "descripcion,motivo,estado";
         
-        sql_insert += ") VALUES ('" + reclamo.getTicket()
-                + "','" + reclamo.getFecha()
+        sql_insert += ") VALUES (DEFAULT"
+                + ",'" + reclamo.getFecha()
                 + "','" + reclamo.getDescripcion()
                 + "','" + reclamo.getMotivo()
                 + "','" + reclamo.getEstado()               
                 +"')";
 
+        System.err.println(sql_insert);
         int result = 0;
         try {
             Connection conn = fachada.conectar();
             Statement sentence = conn.createStatement();
             result = sentence.executeUpdate(sql_insert);
             fachada.cerrarConexion(conn);
-            JOptionPane.showMessageDialog(null, "Inserte Reclamo");
+            System.out.println( "Inserte Reclamo");
         } catch (SQLException se) {
-                        JOptionPane.showMessageDialog(null, se.toString());
+                       // JOptionPane.showMessageDialog(null, se.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
         return result; //result
     }
+     
+     
+     public int lastTicketId()
+     {
+     
+         int ticketNumber = 0;
+         
+         String sql_count = "SELECT * FROM reclamo ORDER BY ticket DESC LIMIT 1";
+         
+         try
+         {
+         
+             Connection conn = fachada.conectar();
+             Statement sentence = conn.createStatement();
+             ResultSet table = sentence.executeQuery(sql_count);
+             
+             while(table.next())
+             {
+                 ticketNumber = Integer.parseInt(table.getString(1));
+             
+             }      
+             
+         
+         } catch (SQLException se) {
+                       // JOptionPane.showMessageDialog(null, se.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+     
+         return ticketNumber;
+     
+     }
+     
+     
     
 }
