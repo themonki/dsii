708b95fbef37d3f636673fb70f5bc48266c544af
felipex <felipex1991@gmail.com>
2012-02-13 18:00:28 -0500

    tarjetas

--- SIGETI/src/java/Dao/DaoCard.java
+++ SIGETI/src/java/Dao/DaoCard.java
@@ -45,15 +45,16 @@ public class DaoCard {
         return result;
         
     }
-     public int saveCard(TarjetaGenerica tarjeta) {
-
-        int result=0;
-        String sql_insert = "INSERT INTO tarjeta  (pin,saldo,estado,tipo,costo,fecha_venta,estacion_venta)";
+    
+    private int saveCardCustom(String pin){
         
-        sql_insert+="VALUES ("+ tarjeta.getPin()+"  ,"+tarjeta.getSaldo()+",true"+ ","+tarjeta.getTipo()+ ","+tarjeta.getCosto()+ ","+"null"+ ","+tarjeta.getEstacionVenta()+")";
+         int result=0;
+        String sql_insert = "INSERT INTO tarjeta_personalizada   (pin,credito)";
+        
+        sql_insert+="VALUES ("+pin+ ",3)";
 
       
-         System.err.println(sql_insert);
+         System.err.println("ENTRE A PERSONA"+sql_insert);
         try {
             Connection conn = fachada.conectar();
             Statement sentence = conn.createStatement();
@@ -65,6 +66,73 @@ public class DaoCard {
             e.printStackTrace();
         }
         System.err.println(sql_insert);
+        return result;
+     
+    }
+        
+    private int saveCardGeneric(String pin){
+        
+         int result=0;
+        String sql_insert = "INSERT INTO tarjeta_generica  (pin)";
+        
+        sql_insert+="VALUES ("+pin+ ")";
+
+      
+         System.err.println("ENTRE A GENERICA"+sql_insert);
+        try {
+            Connection conn = fachada.conectar();
+            Statement sentence = conn.createStatement();
+            result = sentence.executeUpdate(sql_insert);
+            fachada.cerrarConexion(conn);
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.err.println(sql_insert);
+        return result;
+     
+    }
+     public int saveCard(TarjetaGenerica tarjeta) {
+
+        int result=0;
+        String sql_insert = "INSERT INTO tarjeta  (pin,saldo,estado,tipo,costo,fecha_venta,estacion_venta)";
+        
+        sql_insert+="VALUES ("+ tarjeta.getPin()+"  ,"+tarjeta.getSaldo()+",true"+ ","+tarjeta.getTipo()+ ","+tarjeta.getCosto()+ ","+"null"+ ","+tarjeta.getEstacionVenta()+")";
+
+       
+       
+       
+         System.err.println(sql_insert);
+        try {
+            Connection conn = fachada.conectar();
+            Statement sentence = conn.createStatement();
+            result = sentence.executeUpdate(sql_insert);
+            fachada.cerrarConexion(conn);
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        
+       System.err.println("antes de condciion ");
+        if( tarjeta.getTipo()==2){
+           saveCardGeneric( tarjeta.getPin());
+       
+       
+     
+        }
+       
+     
+        if( tarjeta.getTipo()==1){
+           saveCardCustom(tarjeta.getPin());
+       
+       
+    
+        }
+        
+        System.err.println("despues de condciion ");
         return result;
         
     }
@@ -112,6 +180,40 @@ public class DaoCard {
         
         System.err.println("AQUIIIIIIIIIIIIIII:::" +pin);
         return tarjeta;
+     }
+     
+     
+     public String findCardFromUser(String cedula ){
+         String sqlConsulta = "SELECT adquiere_tarjeta from usuario WHERE id = '" + cedula+"'" ;
+         
+          int a;
+          String pin="";
+        
+
+        try {
+            Connection conn = fachada.conectar();
+            Statement sentence = conn.createStatement();
+            ResultSet table = sentence.executeQuery(sqlConsulta);
+            
+
+            while (table.next()) { 
+                
+                
+                pin=table.getString("adquiere_tarjeta");
+                
+            }
+            fachada.cerrarConexion(conn);
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+         
+         
+         
+         return pin ;
+     
+     
      }
     
     
